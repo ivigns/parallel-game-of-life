53e6f7b153303f76e20404c4d40570d9cce6dfce
Ivan Ignatev <ignatev.is@phystech.edu>
2018-12-19 19:21:18 +0300

    Fixes

--- gol-mpi/game_of_life.cpp
+++ gol-mpi/game_of_life.cpp
@@ -308,6 +308,7 @@ void GameOfLife::SlaveSynchronize() {
         }
       }
     } else {
+      std::cout << world_rank_ << " ready.\n";
       SlaveRecv(border_gained, quit, notify_master);
     }
   }
